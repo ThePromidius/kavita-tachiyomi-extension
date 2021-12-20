fdd3582404dc25c4d6300d7c79cc96321afb4207
ThePromidius <thepromidiusyt@gmail.com>
2021-12-20 17:29:37 +0100

    Manga details fixed: Artist,Author,Summary, ¿genres? should be displayed properly now

--- src/all/kavita/src/eu/kanade/tachiyomi/extension/all/kavita/Kavita.kt
+++ src/all/kavita/src/eu/kanade/tachiyomi/extension/all/kavita/Kavita.kt
@@ -222,9 +222,11 @@ class Kavita : ConfigurableSource, HttpSource() {
         if (existingSeries != null) {
             println("Found existing series")
             val manga = helper.createSeriesDto(existingSeries, baseUrl)
-            manga.artist = result.artists.joinToString { ", " }
-            manga.author = result.writers.joinToString { ", " }
-            manga.genre = result.genres.joinToString { ", " }
+            manga.artist = result.artists.joinToString { "${it.name}" }
+            manga.description = result.summary
+            manga.author = result.writers.joinToString { "${it.name}" }
+            manga.genre = result.genres.joinToString { "${it.title}" }
+
             return manga
         }
 

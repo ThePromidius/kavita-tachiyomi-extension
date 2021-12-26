4b25006c293ed48ecb52cc8c2407e98558baf77e
ThePromidius <thepromidiusyt@gmail.com>
2021-12-26 23:29:58 +0100

    removed unused import

--- src/all/kavita/src/eu/kanade/tachiyomi/extension/all/kavita/Kavita.kt
+++ src/all/kavita/src/eu/kanade/tachiyomi/extension/all/kavita/Kavita.kt
@@ -45,7 +45,6 @@ import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
-import org.json.JSONObject
 import rx.Observable
 import rx.Single
 import rx.schedulers.Schedulers
